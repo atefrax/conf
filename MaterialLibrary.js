d42a9eddb1d740df27f300557f6e6560bec48cf2
atefrax <84969801+atefrax@users.noreply.github.com>
2023-05-25 16:23:46 +0200

    v1

---
+++ MaterialLibrary.js
@@ -0,0 +1,147 @@
+var MaterialLibrary = {
+	'Pillows': {
+		'hexa': {
+			DiffusePBR_color: [149.94,149.94,149.94],
+			DiffusePBR_texture_name: "Estelio_Calipso_difuse2.jpg",
+			DiffusePBR_factor: 1.0,
+			SpecularPBR_color: [229.5,229.5,229.5],
+			SpecularPBR_factor: 0.0,
+			GlossinessPBR_factor: 0.1,
+			Opacity_factor: 1.0,
+			EmitColor_factor: 0.0,
+			EmitColor_color: [149.94,149.94,149.94]
+		},
+		'red hexa': {
+			DiffusePBR_color: [149.94,149.94,149.94],
+			DiffusePBR_texture_name: "Estelio_Calipso_difuse3.jpg",
+			DiffusePBR_factor: 1.0,
+			SpecularPBR_color: [229.5,229.5,229.5],
+			SpecularPBR_factor: 0.0,
+			GlossinessPBR_factor: 0.1,
+			Opacity_factor: 1.0,
+			EmitColor_factor: 0.0,
+			EmitColor_color: [149.94,149.94,149.94]
+		},
+		'cercles': {
+			DiffusePBR_color: [149.94,149.94,149.94],
+			DiffusePBR_texture_name: "Estelio_Calipso_difuse4.jpg",
+			DiffusePBR_factor: 1.0,
+			SpecularPBR_color: [229.5,229.5,229.5],
+			SpecularPBR_factor: 0.0,
+			GlossinessPBR_factor: 0.1,
+			Opacity_factor: 1.0,
+			EmitColor_factor: 0.0,
+			EmitColor_color: [149.94,149.94,149.94]
+		},
+		'blue hexa': {
+			DiffusePBR_color: [149.94,149.94,149.94],
+			DiffusePBR_texture_name: "Estelio_Calipso_difuse5.jpg",
+			DiffusePBR_factor: 1.0,
+			SpecularPBR_color: [229.5,229.5,229.5],
+			SpecularPBR_factor: 0.0,
+			GlossinessPBR_factor: 0.1,
+			Opacity_factor: 1.0,
+			EmitColor_factor: 0.0,
+			EmitColor_color: [149.94,149.94,149.94]
+		},
+		'waves': {
+			DiffusePBR_color: [149.94,149.94,149.94],
+			DiffusePBR_texture_name: "Estelio_Calipso_difuse6.jpg",
+			DiffusePBR_factor: 1.0,
+			SpecularPBR_color: [229.5,229.5,229.5],
+			SpecularPBR_factor: 0.0,
+			GlossinessPBR_factor: 0.1,
+			Opacity_factor: 1.0,
+			EmitColor_factor: 0.0,
+			EmitColor_color: [149.94,149.94,149.94]
+		}
+	},
+	'Fabrics': {
+		'sharp	': {
+			DiffusePBR_color: [149.94,149.94,149.94],
+			DiffusePBR_texture_name: "Estelio_Calipso_difuse1.jpg",
+			DiffusePBR_factor: 1.0,
+			SpecularPBR_color: [229.5,229.5,229.5],
+			SpecularPBR_factor: 0.0,
+			GlossinessPBR_factor: 0.1,
+			Opacity_factor: 1.0,
+			EmitColor_factor: 0.0,
+			EmitColor_color: [149.94,149.94,149.94]
+		},
+		'brown': {
+			DiffusePBR_color: [149.94,149.94,149.94],
+			DiffusePBR_texture_name: "Estelio_Calipso_difuse_brown.jpg",
+			DiffusePBR_factor: 1.0,
+			SpecularPBR_color: [229.5,229.5,229.5],
+			SpecularPBR_factor: 0.0,
+			GlossinessPBR_factor: 0.1,
+			Opacity_factor: 1.0,
+			EmitColor_factor: 0.0,
+			EmitColor_color: [149.94,149.94,149.94]
+		},
+		'red': {
+			DiffusePBR_color: [149.94,149.94,149.94],
+			DiffusePBR_texture_name: "Estelio_Calipso_difuse_red.jpg",
+			DiffusePBR_factor: 1.0,
+			SpecularPBR_color: [229.5,229.5,229.5],
+			SpecularPBR_factor: 0.0,
+			GlossinessPBR_factor: 0.1,
+			Opacity_factor: 1.0,
+			EmitColor_factor: 0.0,
+			EmitColor_color: [149.94,149.94,149.94]
+		},
+		'sky': {
+			DiffusePBR_color: [149.94,149.94,149.94],
+			DiffusePBR_texture_name: "Estelio_Calipso_difuse_sky.jpg",
+			DiffusePBR_factor: 1.0,
+			SpecularPBR_color: [229.5,229.5,229.5],
+			SpecularPBR_factor: 0.0,
+			GlossinessPBR_factor: 0.1,
+			Opacity_factor: 1.0,
+			EmitColor_factor: 0.0,
+			EmitColor_color: [149.94,149.94,149.94]
+		},
+		'teal': {
+			DiffusePBR_color: [149.94,149.94,149.94],
+			DiffusePBR_texture_name: "Estelio_Calipso_difuse_teal.jpg",
+			DiffusePBR_factor: 1.0,
+			SpecularPBR_color: [229.5,229.5,229.5],
+			SpecularPBR_factor: 0.0,
+			GlossinessPBR_factor: 0.1,
+			Opacity_factor: 1.0,
+			EmitColor_factor: 0.0,
+			EmitColor_color: [149.94,149.94,149.94]
+		}
+	},
+	'legs': {
+		'Black legs': {
+			DiffusePBR_color: [2.0,2.0,2.0],
+			SpecularPBR_color: [229.5,229.5,229.5],
+			SpecularPBR_factor: 0.97,
+			GlossinessPBR_factor: 0.52,
+			Opacity_factor: 1.0,
+			EmitColor_factor: 0.0,
+			EmitColor_color: [2.0,2.0,2.0]
+		},
+		'Chrome Legs': {
+			DiffusePBR_color: [149.94,149.94,149.94],
+			SpecularPBR_color: [229.5,229.5,229.5],
+			SpecularPBR_factor: 0.0,
+			GlossinessPBR_factor: 0.1,
+			Opacity_factor: 1.0,
+			EmitColor_factor: 0.0,
+			EmitColor_color: [149.94,149.94,149.94]
+		},
+		'Wood Legs': {
+			DiffusePBR_color: [119.0,119.0,119.0],
+			DiffusePBR_texture_name: "vmppmax_v4_9_1_9_d.jpg",
+			DiffusePBR_factor: 1.0,
+			SpecularPBR_color: [0.0,0.0,0.0],
+			SpecularPBR_factor: 0.42,
+			GlossinessPBR_factor: 0.0,
+			Opacity_factor: 1.0,
+			EmitColor_factor: 0.0,
+			EmitColor_color: [119.0,119.0,119.0]
+		}
+	}
+}
---
+++ ObjectSet.js
@@ -0,0 +1,14 @@
+var ObjectSets = {
+	'Hpouf': {
+		'Off': {Default: false},
+		'On': {Default: true}
+	},
+	'Hright': {
+		'Off': {Default: false},
+		'On': {Default: true}
+	},
+	'Hcorner': {
+		'Off': {Default: false},
+		'On': {Default: true}
+	}
+}
